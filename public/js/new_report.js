04b722c82371f6a5d0963d1c40a47271ab8adb15
Washington Sampaio Vieirar <washington@primi.com.br>
2019-12-12 08:48:23 -0300

    Testando o deploy da heroku

--- public/js/new_report.js
+++ public/js/new_report.js
@@ -2,17 +2,17 @@ var trello = TrelloPowerUp.iframe();
 
 resize(trello, '#newReport')
 
-window.newReport.addEventListener('submit', function(event) {
+window.newReport.addEventListener('submit', function (event) {
     event.preventDefault();
     var card = {};
 
     getCardDetailsById(trello)
-        .then(function(serializedCard) {
+        .then(function (serializedCard) {
             card = JSON.stringify(serializedCard);
             console.log(card);
             addReport(card);
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log(error);
             alert('Ocorreu um erro, por favor tente novamente mais tarde');
         });
@@ -27,22 +27,24 @@ function addReport(card) {
     var membersIds = card.members;
     var title = card.name;
     var currDate = new Date();
+    var startDate = window.startDate.value;
 
-    formatedCurrDate = (currDate.getDate().length = 1? "0": "") + currDate.getDate() + "/" + currDate.getMonth() + "/" + currDate.getFullYear();
+    formatedCurrDate = (currDate.getDate().length = 1 ? "0" : "") + currDate.getDate() + "/" + currDate.getMonth() + "/" + currDate.getFullYear();
 
-    if(startTime && endTime && comment) {
-        if(endTime > startTime) {
+    if (startTime && endTime && comment) {
+        if (endTime > startTime) {
             var report = {
                 'date': formatedCurrDate,
                 'title': title,
                 'card': cardURL,
                 'membersIds': membersIds,
                 'startTime': startTime,
+                'startDate': startDate,
                 'endTime': endTime,
                 'commitLink': commitLink,
                 'comment': comment
             };
-            
+
             return addNewReport(trello, report)
         }
         else alert('O tempo de início deve ser menor que o tempo de fim da tarefa.');
