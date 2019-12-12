c123d05e2e203a11a512eff6b218d3f2ee50dee6
Washington Sampaio Vieirar <washington@primi.com.br>
2019-12-12 09:43:57 -0300

    Deploy

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,25 @@
 TrelloPowerUp.initialize({
-    'card-buttons': function(trello, options) {
+    'card-buttons': function (trello, options) {
         return [{
             icon: 'https://reports-helper.herokuapp.com/icons/reports.svg',
             text: 'Novo relatório',
             callback: showNewReportModal
-        }] 
+        },
+        {
+            icon: 'https://reports-helper.herokuapp.com/icons/attachments.svg',
+            text: 'Exportar relatório',
+            callback: function (trello) {
+                return trello.popup({
+                    title: "Download",
+                    url: 'https://reports-helper.herokuapp.com/type_export.html'
+                })
+            }
+        }]
     },
-    'card-badges': function(trello, options) {
+    'card-badges': function (trello, options) {
         return getReports(trello).then(showBadge)
     },
-    'card-back-section': function(trello, options) {
+    'card-back-section': function (trello, options) {
         return {
             title: 'Relatórios',
             icon: 'https://reports-helper.herokuapp.com/icons/reports.svg',
