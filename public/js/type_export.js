34a7a4eba2c286f8ab67ae634a48f6bcb199dee4
Washington Sampaio Vieirar <washington@primi.com.br>
2019-12-12 10:32:53 -0300

    Deploy

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -1,7 +1,10 @@
 var trello = TrelloPowerUp.iframe();
 
-resize(trello, '#exportData')
+// resize(trello, '#exportData')
 
+t.render(function () {
+    t.sizeTo('#exportData').done();
+});
 
 window.exportData.addEventListener('submit', function (event) {
     alert("clicou")
