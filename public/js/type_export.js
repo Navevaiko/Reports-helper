13a6e33e3e129441a00165918557e95706dada5b
= <=>
2019-12-13 08:05:43 -0300

    Deploy-para-teste

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -9,6 +9,7 @@ trello.render(function () {
 window.exportData.addEventListener('submit', function (event) {
     event.preventDefault()
     alert("clicou");
+    console.log(trello.get('card', 'shared', 'newReport'))
     console.log(trello)
     console.log(trello.exportData)
 
