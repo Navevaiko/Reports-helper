197ad85840c1027b3a3560a4072f4f12e5df87a8
= <=>
2019-12-13 09:49:07 -0300

    Deploy-para-teste

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -10,7 +10,7 @@ window.exportData.addEventListener('submit', async event => {
     event.preventDefault()
 
     json = await trello.getAll()
-    console.log(JSON.parse(json))
+    console.log(json.card)
 
 
 
