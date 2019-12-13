7450ba1ad21e6eb638c8047b2e9762685acb051c
= <=>
2019-12-13 10:27:37 -0300

    Deploy-para-teste

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -9,12 +9,24 @@ trello.render(function () {
 window.exportData.addEventListener('submit', async event => {
     event.preventDefault()
 
-    json = await trello.getAll()
-    console.log(JSON.parse(json).card)
+    json = await JSON.parse(trello.getAll())
 
+    let nameFile = window.typeData.value
 
-
+    download(`data${nameFile}`, JSON.stringify(json.card.shared))
 
     trello.closePopup();
 });
 
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
