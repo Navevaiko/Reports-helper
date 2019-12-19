8b3077c1c9f2982886d38262f042b1dfd1286a28
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2019-12-19 08:42:16 -0300

    realizando o deploy para teste

--- public/js/reports_list.js
+++ public/js/reports_list.js
@@ -31,32 +31,60 @@ const loadReportsList = reports => {
 
 const createListReportElement = reportDatas => reportDatas.map(e => createReportElementToPdf(e));
 
-const createReportElementToPdf = reportData => {
-    let attachmentsElement = "<object type='image/svg+xml' data='/icons/attachments.svg'> Anexos </object>";
-    let mainElement = "";
-    mainElement = `<li class='report'> 
-                        <p id="txt_info">Comentário: ${reportData.comment} </p> 
-                        <p>Link do commit: ${reportData.commitLink}</p>
-                        ${(reportData.attachments ? attachmentsElement : "")}
-                        <div class='datetimeInfo'> 
-                            <span> ${reportData.date} </span> 
-                            <span> ${reportData.startTime} ás ${reportData.endTime} </span> 
-                        </div>
-                        <div class='datetimeInfo'> 
-                            Cartão: ${reportData.title}
-                        </div>
-                        <div class='datetimeInfo'> 
-                            Membros: ${reportData.members}
-                        </div>
-                        <div class='datetimeInfo'> 
-                            Etiquetas: ${reportData.labels}
-                        </div>
-                        <div class='datetimeInfo'> 
-                            Link do cartão: ${reportData.card}
-                        </div>
-                    </li><hr/>`;
 
-    return mainElement;
+
+
+
+
+const createReportElementToPdf = async reportData => {
+    let tags = reportData.labels.map(e => `<div class="tags" style="background-color: ${e.color};">${e.name}</div>`)
+    let members = reportData.members.map(e => `<div class="name_member">${e.fullName}</div><div class="perfil_member" style="background-image: ${e.avatar};"></div><br>`);
+    let fullTime = "diferença de tempo aqui";
+
+    let boxHtml = `<div class="report_container">
+                    <!-- delimitador 1-->
+                    <div class="item_body member">
+                        <div class="div_caixa">
+                            ${members.join('')}
+                        </div>
+                    </div>
+                    <!-- delimitador 2-->
+                    <div class="item_body card">
+                        ${reportData.name}
+                    </div>
+                    <!-- delimitador 3-->
+                    <div class="item_body tag">
+                        ${tags.join('')}
+                    </div>
+                    <!-- delimitador 4-->
+                    <div class="item_body date">
+                        ${reportData.date}<br>
+                        ${reportData.startTime} às 
+                        ${reportData.endTime}
+                    </div>
+                    <!-- delimitador 5-->
+                    <div class="item_body start_end">
+                        ${fullTime}
+                    </div>
+                    <!-- delimitador 6-->
+                    <div class="item_body link">
+                        <div class="link_trello">
+                            <a class="trello" href="${reportData.commitLink}">
+                                Trello:
+                            <a>
+                            <br>
+                            <a class="Commit"
+                                href="${reportData.cardURL}">
+                                Commit
+                            </a>
+                        </div>
+                    </div>
+                    <!-- delimitador 7-->
+                    <div class="item_body comment">
+                    ${reportData.comment}
+                    </div>
+                </div>`;
+    return boxHtml;
 }
 
 const createReportElement = reportData => {
