c2a371d1ca771096742c82979d1cfe50fcb1b7c9
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2019-12-20 09:05:30 -0300

    Refatorando o codigo para deixar mais facil as manutencoes

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -1,6 +1,5 @@
 var trello = TrelloPowerUp.iframe();
 
-
 trello.render(function () {
     trello.sizeTo('#exportData').done();
 });
@@ -9,8 +8,8 @@ window.exportData.addEventListener('submit', async event => {
 
     event.preventDefault()
 
-    let cardContent = await getCardContent(trello);
-    let card = await getCardDetailsById(trello);
+    const cardContent = await getCardContent(trello);
+    const card = await getCardDetailsById(trello);
 
     let dataCard = getDataCardExport(cardContent.card, card);
 
@@ -28,80 +27,81 @@ window.exportData.addEventListener('submit', async event => {
 
 });
 
-async function downloadByType(type, json) {
+const downloadByType = (type, json) => {
     console.log(json)
 
 
     switch (type) {
         case "CSV":
-            json.labels = json.labels.join('')
-            json.members = json.members.join('')
-            JSONToCSVConvertor(json, "Relatorio.csv", true)
+            json.labels = json.labels.join('');
+            json.members = json.members.join('');
+
+            JSONToCSVConvertor(json, "Relatorio.csv", true);
 
             break;
         case "JSON":
-            download(`Relatorio.json`, JSON.stringify(json))
+            download(`Relatorio.json`, JSON.stringify(json));
 
             break;
         default:
         case "PDF":
             let content = createListReportElement(json).join('');
-
-            var mywindow = window.open('', 'Print', 'height=600,width=800');
-
-            mywindow.document.write(`<!DOCTYPE html>
-                                        <html lang="en">
-                                        <head>
-                                            <meta charset="UTF-8">
-                                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                                            <link rel="stylesheet" type="text/css" href="https://reports-helper.herokuapp.com/reportstyle">
-                                            <title>Relátorio</title>
-                                        </head><body>`);
-
-            mywindow.document.write(`<script>
-                                        setTimeout(() => { window.print(); window.close(); }, 1500);
-                                    </script>
-                                    <div class="container">
-                                        <!--delimitador do header-->
-                                        <div class="container_header">
-                                            <div class="item_header member">
-                                                Membros
-                                            </div>
-                                            <div class="item_header card">
-                                                Cartão
-                                            </div>
-                                            <div class="item_header tag">
-                                                Etiquetas
-                                            </div>
-                                            <div class="item_header date">
-                                                Data
-                                            </div>
-                                            <div class="item_header start_end">
-                                                Duração
-                                            </div>
-                                            <div class="item_header link">
-                                                Links
-                                            </div>
-                                            <div class="item_header comment">
-                                                Comentário
-                                            </div>
-                                        </div>
-                                        `);
-            mywindow.document.write(content);
-            ywindow.document.write(`</body></html>`);
-
-            mywindow.document.close();
-
-
+            openWindowForPdf(content);
 
             break;
     }
 
 }
 
-function download(filename, text) {
-    var element = document.createElement('a');
+const openWindowForPdf = content => {
+    var mywindow = window.open('', 'Print', 'height=900,width=1100');
+
+    mywindow.document.write(`<!DOCTYPE html>
+                                <html lang="en">
+                                <head>
+                                    <meta charset="UTF-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                                    <link rel="stylesheet" type="text/css" href="https://reports-helper.herokuapp.com/reportstyle">
+                                <title>Relátorio</title>
+                            </head><body>`);
+
+    mywindow.document.write(`<script>
+                                setTimeout(() => { window.print(); window.close(); }, 1500);
+                            </script>
+                            <div class="container">
+                                <!--delimitador do header-->
+                                <div class="container_header">
+                                    <div class="item_header member">
+                                        Membros
+                                    </div>
+                                    <div class="item_header card">
+                                        Cartão
+                                    </div>
+                                    <div class="item_header tag">
+                                        Etiquetas
+                                    </div>
+                                    <div class="item_header date">
+                                        Data
+                                    </div>
+                                    <div class="item_header start_end">
+                                        Duração
+                                    </div>
+                                    <div class="item_header link">
+                                        Links
+                                    </div>
+                                    <div class="item_header comment">
+                                        Comentário
+                                    </div>
+                                </div>`);
+    mywindow.document.write(content);
+    ywindow.document.write(`</body></html>`);
+    mywindow.document.close();
+}
+
+const download = (filename, text) => {
+    let element = document.createElement('a');
+
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
 
@@ -113,8 +113,7 @@ function download(filename, text) {
     document.body.removeChild(element);
 }
 
-
-function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+const JSONToCSVConvertor = (JSONData, ReportTitle, ShowLabel) => {
     var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
 
     var CSV = '';
