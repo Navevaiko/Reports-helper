e38741a45f711c4b2a2e4c86e4b6819aeb7f8804
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2019-12-23 14:40:16 -0300

    Deploy-para-teste

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -30,7 +30,19 @@ window.exportData.addEventListener('submit', async event => {
 
         let cards = await axios.get(`https://api.trello.com/1/boards/${idBoard}/cards/?fields=name,labels,members,plugindata&members=true&key=${secret}&token=${token}`);
 
-        console.log(cards);
+        dataCard = cards.data.map(async card => {
+            const { id } = card;
+
+            const resquest = await axios.get(`https://api.trello.com/1/cards/${id}/pluginData?key=${secret}&token=${token}`);
+
+            console.log(request);
+
+
+        })
+
+
+
+
 
 
         // dataCard =  cards.data.map( card => {
