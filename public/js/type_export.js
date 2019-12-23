8f042174a91b50982cb2eddc5915b551e7b81c59
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2019-12-23 13:41:49 -0300

    Deploy-para-teste

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -30,7 +30,7 @@ window.exportData.addEventListener('submit', async event => {
 
         let cards = await axios.get(`https://api.trello.com/1/boards/${idBoard}/cards/?fields=name,labels,members,plugindata&members=true&key=${secret}&token=${token}`);
 
-        dataCard = cards.data.map(card => inflateDataCard(card, secret, token));
+        dataCard = await cards.data.map(async card => await inflateDataCard(card, secret, token));
     }
 
     Promise.all(dataCard)
