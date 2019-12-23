0b6b3bdf898d5b42231af09353caaf0ec5bd07a7
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2019-12-23 07:56:05 -0300

    Deploy-para-teste

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,4 @@
-
 const generateReportMaFromBoard = () => alert('OI')
-
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
-
-var onBtnClick = function (t, opts) {
-    console.log('Someone clicked the button');
-};
 
 window.TrelloPowerUp.initialize({
     'card-buttons': function (trello, options) {
@@ -34,38 +26,15 @@ window.TrelloPowerUp.initialize({
             }
         }
     },
-    // 'board-buttons': function (trello, options) {
-    //     return {
-    //         icon: {
-    //             dark: "https://reports-helper.herokuapp.com/icons/reportdark.svg",
-    //             light: "https://reports-helper.herokuapp.com/icons/reportligth.svg"
-    //         },
-    //         text: 'TESETE',
-    //         callback: generateReportMaFromBoard,
-    //         condition: 'always'
-    //     }
-    // }
-    'board-buttons': function (t, opts) {
-        return [{
-            // we can either provide a button that has a callback function
+    'board-buttons': function (trello, options) {
+        return {
             icon: {
-                dark: WHITE_ICON,
-                light: BLACK_ICON
+                dark: "https://reports-helper.herokuapp.com/icons/reportdark.svg",
+                light: "https://reports-helper.herokuapp.com/icons/reportligth.svg"
             },
-            text: 'Callback',
-            callback: onBtnClick,
-            condition: 'edit'
-        }, {
-            // or we can also have a button that is just a simple url
-            // clicking it will open a new tab at the provided url
-            icon: {
-                dark: WHITE_ICON,
-                light: BLACK_ICON
-            },
-            text: 'URL',
-            condition: 'always',
-            url: 'https://trello.com/inspiration',
-            target: 'Inspiring Boards' // optional target for above url
-        }];
+            text: 'TESETE',
+            callback: generateReportMaFromBoard,
+            condition: 'always'
+        }
     }
 });
