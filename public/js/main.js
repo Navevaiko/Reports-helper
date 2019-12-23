fe0a44078332feee3423f420d887fc4bcb0f7b9a
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2019-12-23 09:08:37 -0300

    Deploy-para-teste

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,14 @@
 const generateReportMaFromBoard = async (trello, options) => {
 
-    let get = trello.get();
-    let get2 = trello.get('board', 'shared')
-    let getall = trello.getall();
-    let card = trello.card('all');
+    let get1 = await trello.get();
+    let get2 = await trello.get('board', 'shared')
+    let getall = await trello.getall();
+    let card = await trello.card('all');
 
-
-
-
-    console.log(get);
+    console.log(get1);
     console.log(get2);
     console.log(getall);
     console.log(card);
-
-
-
 }
 
 window.TrelloPowerUp.initialize({
