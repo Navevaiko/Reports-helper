dbd3eba08b6607b447c5158a8b50d1da838ed606
washingtonSampaioVieira <washingtonsampaio.23@gmail.com>
2020-01-06 13:29:51 -0300

    Deploy-para-teste

--- public/js/trello_functions.js
+++ public/js/trello_functions.js
@@ -45,14 +45,9 @@ const getReports = trello => {
 const getReportsAnyKy = async trello => {
     let getAll = await trello.getAll();
 
-
-    console.log(getAll["_settledValue"].card.shared)
-
-
-
+    console.log(getAll["_settledValue"])
 
     concatMyKeyInObj(getAll)
-
 }
 
 const concatMyKeyInObj = reports => {
