67041c32708cf968a7e19f87ca92d54a39cbe924
Erick Araujo <erick@primi.com.br>
2020-02-28 13:55:12 -0300

    Testes de console.log

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -109,7 +109,7 @@ const requestReports = async (card, token, secret) => {
 
     const request = await axios.get(`https://api.trello.com/1/cards/${id}/pluginData?key=${secret}&token=${token}`);
 
-    console.log(request)
+    console.log(request.data)
 
     let json = !!request.data[0] ? request.data[0].value : "";
 
