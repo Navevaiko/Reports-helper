78533726ef09f66e7c9409ee403f4072020d8599
Erick Araujo <erick@primi.com.br>
2020-03-02 16:42:29 -0300

    Mostrar relatório criado na lista após sua criação dentro do card

--- public/js/trello_functions.js
+++ public/js/trello_functions.js
@@ -9,6 +9,24 @@ const showNewReportModal = trello => {
     })
 }
 
+const createReportElement = reportData => {
+    let attachmentsElement = "<object type='image/svg+xml' data='/icons/attachments.svg'> Anexos </object>";
+    let mainElement = "";
+
+    mainElement = `<li class='report'> 
+                        <p id=\txt_info\">   ${reportData.comment} </p> 
+                        <span><a href='  ${reportData.commitLink}' target='_blank'> Commit </a></span>
+                        ${(reportData.attachments ? attachmentsElement : "")}
+                        <div class='datetimeInfo'> 
+                            <span> ${reportData.currDate} </span> 
+                            <span> ${reportData.startTime} ás ${reportData.endTime} </span> 
+                        </div>
+                        <div class="remove_report" id="${reportData.key}">X</div>
+                    </li><hr/>`;
+
+    return mainElement;
+}
+
 const showConfirmationDialogue = trello => {
     return trello.popup({
         title: "Excluir relátorio?",
@@ -45,23 +63,6 @@ const addNewReport = (trello, report) => {
 
                 window.reportsList.innerHTML = reportsElementsList;
 
-                const createReportElement = reportData => {
-                    let attachmentsElement = "<object type='image/svg+xml' data='/icons/attachments.svg'> Anexos </object>";
-                    let mainElement = "";
-                
-                    mainElement = `<li class='report'> 
-                                        <p id=\txt_info\">   ${reportData.comment} </p> 
-                                        <span><a href='  ${reportData.commitLink}' target='_blank'> Commit </a></span>
-                                        ${(reportData.attachments ? attachmentsElement : "")}
-                                        <div class='datetimeInfo'> 
-                                            <span> ${reportData.currDate} </span> 
-                                            <span> ${reportData.startTime} ás ${reportData.endTime} </span> 
-                                        </div>
-                                        <div class="remove_report" id="${reportData.key}">X</div>
-                                    </li><hr/>`;
-                
-                    return mainElement;
-                }
             })
             .catch(function (error) {
                 alert("Ocorreu um erro, por favor tente novamente mais tarde");
