0ab46eef4d0ecf660f726043cae87c48a559f35e
Erick Araujo <erick@primi.com.br>
2020-03-02 14:34:06 -0300

    Testes de console.log

--- public/js/type_export.js
+++ public/js/type_export.js
@@ -104,15 +104,8 @@ const requestReports = async (card, token, secret) => {
         const jsonUnified = json.map(e => ({ ...e, title: card.name, members: card.members, labels: card.labels }))
 
         if(jsonUnified[0].comment){
-            console.log(jsonUnified)
-        } else {
-            console.log("Não possui comentário")
+            arrayUnified.push(jsonUnified)
         }
-
-        console.log("comentario: ")
-        console.log(jsonUnified[0].comment)
-
-        arrayUnified.push(jsonUnified)
     } 
 
     return arrayUnified;
