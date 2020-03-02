27992308f1c62601ce6ed21e69ac2fd78a1ccbc2
Erick Araujo <erick@primi.com.br>
2020-03-02 17:03:58 -0300

    Inserção do novo relatório na lista do card

--- public/js/trello_functions.js
+++ public/js/trello_functions.js
@@ -1,6 +1,24 @@
 var reportsKey = 'reports';
 var reportsVisibility = 'shared'
 
+const createReportElement = reportData => {
+    let attachmentsElement = "<object type='image/svg+xml' data='/icons/attachments.svg'> Anexos </object>";
+    let mainElement = "";
+
+    mainElement = `<li class='report'> 
+                        <p id=\txt_info\">   ${reportData.comment} </p> 
+                        <span><a href='  ${reportData.commitLink}' target='_blank'> Commit </a></span>
+                        ${(reportData.attachments ? attachmentsElement : "")}
+                        <div class='datetimeInfo'> 
+                            <span> ${reportData.currDate} </span> 
+                            <span> ${reportData.startTime} ás ${reportData.endTime} </span> 
+                        </div>
+                        <div class="remove_report" id="${reportData.key}">X</div>
+                    </li><hr/>`;
+
+    return mainElement;
+}
+
 const showNewReportModal = trello => {
     return trello.modal({
         title: 'Novo relatório',
