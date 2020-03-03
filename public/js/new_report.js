189f58b2510e592a264743ba43ab0ff68195bf7a
Erick Araujo <erick@primi.com.br>
2020-03-03 13:54:08 -0300

    Melhoras de identação e clean code

--- public/js/new_report.js
+++ public/js/new_report.js
@@ -29,30 +29,51 @@ const addReport = card => {
     let currDate = toDate(window.startDate.value);
 
     dateSplit = currDate.split('/')
+    
+    if (!startTime && !endTime && !comment)
+        return alert('Preencha todos os campos!')
 
-    currDia = dateSplit[0]; currMes = dateSplit[1]; currAno = dateSplit[2];
+    if(dateSplit[0] == 'undefined' || dateSplit[1] == 'undefined' || dateSplit[2] == "") 
+        return alert("Insira uma data em 'Início da tarefa'")
 
-    if (startTime && endTime && comment) {
+    if(startTime > endTime)
+        return alert('O tempo de início deve ser menor que o tempo de fim da tarefa.')
+        
+    let report = {
+        currDate,
+        title,
+        cardURL,
+        membersIds,
+        startTime,
+        endTime,
+        commitLink,
+        comment,
+        labels
+    };
 
-        if(currDia == 'undefined' || currMes == 'undefined' || currAno == "") return alert("Insira uma data em 'Início da tarefa'")
-
-        if (endTime > startTime) {
-            let report = {
-                currDate,
-                title,
-                cardURL,
-                membersIds,
-                startTime,
-                endTime,
-                commitLink,
-                comment,
-                labels
-            };
-
-            return addNewReport(trello, report)
-        } else {
-            alert('O tempo de início deve ser menor que o tempo de fim da tarefa.');
-        }
-    }
-    else alert("Preencha todos os campos!")
+    return addNewReport(trello, report)
 }
+
+// if (startTime && endTime && comment) {
+
+//     if(currDia == 'undefined' || currMes == 'undefined' || currAno == "") return alert("Insira uma data em 'Início da tarefa'")
+
+//     if (endTime > startTime) {
+//         let report = {
+//             currDate,
+//             title,
+//             cardURL,
+//             membersIds,
+//             startTime,
+//             endTime,
+//             commitLink,
+//             comment,
+//             labels
+//         };
+
+//         return addNewReport(trello, report)
+//     } else {
+//         alert('O tempo de início deve ser menor que o tempo de fim da tarefa.');
+//     }
+// }
+// else alert("Preencha todos os campos!")
