f2f13f6e300b5ef9126947917b1c8fffad36f7c7
Erick Araujo <erick@primi.com.br>
2020-03-06 16:27:48 -0300

    Design do relatório

--- public/js/reports_list.js
+++ public/js/reports_list.js
@@ -184,9 +184,7 @@ const createReportElement = reportData => {
     //                     <div class="remove_report" id="${reportData.key}">X</div>
     //                 </li>`;
 
-    // var reduced_commit_details = reportData.commit_details.substring(0,50)
-
-    console.log(reportData)
+    console.log(reportData.commitLink.substring(0,50))
 
     mainElement =  `<li class='li_report'> 
                         <div class="report">
@@ -204,7 +202,7 @@ const createReportElement = reportData => {
                                 </div>
                                 <div class="commit_details">
                                     <h3>Commit: </h3>
-                                    <a href="${reportData.commitLink}" target="_blank"><p class="pCommit">${reportData.commit_details}</p></a>
+                                    <a href="${reportData.commitLink}" target="_blank"><p class="pCommit">${reportData.commitLink}</p></a>
                                 </div>
                             </div>
                             ${(reportData.attachments ? attachmentsElement : "")}
