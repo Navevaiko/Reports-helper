b12dffb88b13c62105df0b77d7dfe76a23f9a153
Erick Araujo <erick@primi.com.br>
2020-03-10 15:53:06 -0300

    change the layout when creating a new report

--- public/js/reports_list.js
+++ public/js/reports_list.js
@@ -5,26 +5,6 @@ window.addEventListener('load', async () => {
     loadReportsList(await getReports(trello))
 });
 
-const btnNewReport = document.getElementById('btn_newReport')
-const formNewReport = document.getElementById('form_newReport')
-
-btnNewReport.addEventListener('click', () => {
-    btnNewReport.style.display = 'none'
-    formNewReport.style.display = 'block'
-})
-
-window.newReport.addEventListener('submit', async event => {
-    console.log("Clicou")
-
-    console.log(event)
-    // event.preventDefault();
-    // var card = {};
-
-    // card = await getCardDetailsById(trello);
-
-    // addReport(JSON.stringify(card));
-});
-
 //Cria a lista para exibição dos elementos no cartão
 const loadReportsList = reports => {
 
@@ -231,4 +211,71 @@ const createReportElement = reportData => {
                     </li>`
 
     return mainElement;
+}
+
+
+// --- NOVO RELATÓRIO --- //
+const btnNewReport = document.getElementById('btn_newReport')
+const formNewReport = document.getElementById('form_newReport')
+
+btnNewReport.addEventListener('click', () => {
+    btnNewReport.style.display = 'none'
+    formNewReport.style.display = 'block'
+})
+
+window.newReport.addEventListener('submit', async event => {
+    event.preventDefault();
+    var card = {};
+
+    card = await getCardDetailsById(trello);
+
+    addReport(JSON.stringify(card));
+});
+
+const toDate = dateStr => {
+    var parts = dateStr.split("-");
+    return `${parts[2]}/${parts[1]}/${parts[0]}`;
+}
+
+const addReport = card => {
+
+    let startTime = window.startTime.value;
+    let endTime = window.endTime.value;
+    let commitLink = window.commitLink.value;
+    let comment = window.comment.value;
+    let cardURL = JSON.parse(card).url;
+    let membersIds = card.members;
+    let title = card.name;
+    let labels = card.labels
+    let currDate = toDate(window.startDate.value);
+
+    return console.log(`${startTime}, ${endTime}, ${commitLink}, ${comment}, ${cardURL}, ${membersIds}, ${title}, ${labels}, ${currDate} `)
+
+    // dateSplit = currDate.split('/')
+
+    // currDia = dateSplit[0]; currMes = dateSplit[1]; currAno = dateSplit[2];
+
+    // if (startTime && endTime && comment) {
+
+    //     if(currDia == 'undefined' || currMes == 'undefined' || currAno == "") return alert("Insira uma data em 'Início da tarefa'")
+
+    //     if (endTime > startTime) {
+    //         let report = {
+    //             currDate,
+    //             title,
+    //             cardURL,
+    //             membersIds,
+    //             startTime,
+    //             endTime,
+    //             commitLink,
+    //             comment,
+    //             labels
+    //         };
+
+    //         return addNewReport(trello, report)
+    //     } else {
+    //         alert('O tempo de início deve ser menor que o tempo de fim da tarefa.');
+    //     }
+    // }
+    // else alert("Preencha todos os campos!")
 }
