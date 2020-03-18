e951c960419c3220b22fc7ee79bc2f4d1ef02ad8
Erick Araujo <erick@primi.com.br>
2020-03-18 16:58:12 -0300

    report editing

--- public/js/reports_list.js
+++ public/js/reports_list.js
@@ -87,7 +87,7 @@ const editReport = elements => {
                     inputCommitLink.value = ""
             }
 
-            localStorage.setItem('id_report', report.parentNode.id)
+            localStorage.setItem('id_report', el.parentNode.id)
         })
     }))
 }
