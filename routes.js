f2ce7b68b814431ab930f163381718747c94536c
Gabriel Navevaiko <gabriel@primi.com.br>
2020-09-01 10:56:37 -0300

    refactor: Refatorando rotas

--- routes.js
+++ routes.js
@@ -2,33 +2,28 @@ const { Router } = require('express');
 
 const router = Router();
 
-router.get('/new_report', (_, response) => {
-    response.sendFile(`${__dirname}/public/views/new_report.html`)
-});
-
-router.get('/reports_list', (_, response) => {
-    response.sendFile(`${__dirname}/public/views/reports_list.html`)
-});
-
-router.get('/download_report', (_, response) => {
-    response.sendFile(`${__dirname}/public/views/download_report.html`)
-});
-
-router.get('/icons/:imageName', (request, response) => {
-    response.sendFile(`${__dirname}/public/icons/${request.params.imageName}`);
-});
-
-router.get('/reportstyle', (_, response) => {
-    response.sendFile(`${__dirname}/public/css/report.css`)
-})
-
-router.get('/confirmation', (_, response) => {
-    response.sendFile(`${__dirname}/public/views/confirm_dialog.html`)
-});
-
-router.get('/', (request, response) => {
+router.get('/', (_, response) => {
     response.sendFile(`${__dirname}/public/views/index.html`)
 });
 
+router.get('/reports/new', (_, response) => {
+    response.sendFile(`${__dirname}/public/views/new_report.html`)
+});
+
+router.get('/reports', (_, response) => {
+    response.sendFile(`${__dirname}/public/views/reports_list.html`)
+});
+
+router.get('/reports/download', (_, response) => {
+    response.sendFile(`${__dirname}/public/views/download_report.html`)
+});
+
+router.get('/assets/:assetName', (request, response) => {
+    response.sendFile(`${__dirname}/public/asset/${request.params.assetName}`);
+});
+
+router.get('/styles/', (_, response) => {
+    response.sendFile(`${__dirname}/public/css/report.css`)
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 require('dotenv/config');
 
-const routes = require('./routes')
 const express = require('express');
 const cors = require('cors');
 
+const routes = require('./routes')
+
 const app = express();
 app.use(cors({ origin: 'https://trello.com' }));
-
 app.use(express.static('public'))
-
 app.use('/', routes)
 
 app.listen(process.env.PORT);
